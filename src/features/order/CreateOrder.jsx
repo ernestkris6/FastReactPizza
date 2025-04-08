a4f0edd019b0d038ebe1cf691be3d5fe4f39c4d7
ERNEST CHRIS <ernestchris6@gmail.com>
2025-04-08 11:11:35 +0100

    fetching orders

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 // import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Loader from "../../ui/Loader";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -48,6 +49,7 @@ function CreateOrder() {
 
   return (
     <div>
+      {/* {isSubmitting && <Loader />} */}
       <h2>Ready to order? Let's go!</h2>
 
       {/* <Form method="POST" action="/order/new"></Form> */}
