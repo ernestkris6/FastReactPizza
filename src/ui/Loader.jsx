e50cb85f13a849df9540d26183d05b5f3c0b1434
ERNEST CHRIS <ernestchris6@gmail.com>
2025-04-14 22:51:18 +0100

    loader

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Loader() {
   return (
-    <div className='loader'></div>
+    <div className='absolute flex text-stone-500 inset-2 justify-center items-center'>
+      <div className='loader'></div>
+    </div>
   )
 }
