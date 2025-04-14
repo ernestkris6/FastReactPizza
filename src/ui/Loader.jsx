fe1be29da7dbf890de3d77fe2662238cb3c3750f
ERNEST CHRIS <ernestchris6@gmail.com>
2025-04-14 23:05:06 +0100

    loader

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Loader() {
   return (
-    <div className='absolute flex text-stone-500 inset-2 justify-center items-center'>
+    <div className='absolute flex bg-stone-500 bg-opacity-20 backdrop-blur-sm inset-0 justify-center items-center'>
       <div className='loader'></div>
     </div>
   )
