83e36ca02d0788c3648fc5a4ed7e4cdda36e61a4
ERNEST CHRIS <ernestchris6@gmail.com>
2025-04-14 22:34:39 +0100

    LinkButton

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function LinkButton({children, to}) {
+
+  const className = 'text-sm text-blue-500 hover:text-blue-300 hover:underline'
+
+  const navigate = useNavigate();
+
+  if(to === '-1') 
+    return <button className={className} onClick={() => navigate(-1)}>{children}</button>
+  
+
   return (
-    <Link to={to} >
+    <Link to={to} className={className}>
         {children}
     </Link>
   )
